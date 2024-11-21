d510b361ec3c12048eda25ce6cf36e0e52063e5f
Asbjrnen <asla0003@stud.kea.dk>
2024-11-21 13:15:15 +0100

    BranchTesting 6

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,12 @@
 public class Main {
+
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        Dog dog = new Dog();
+        Cat cat = new Cat();
+
+        dog.makeSound(); //output "bark"
+        cat.makeSound(); //output "meow"
+
+
     }
 }
